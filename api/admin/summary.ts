8b59ebdc0bb92f47f7559956960f5ba80d2391c8
Giovanni Miranda <giovannimiranda09@gmail.com>
2026-07-06 15:45:11 -0400

    update

--- api/admin/summary.ts
+++
@@ -1,8 +0,0 @@
-import type { IncomingMessage, ServerResponse } from 'node:http';
-import { handleApiRequest } from '../../server/index.js';
-
-export const runtime = 'nodejs';
-
-export default function handler(req: IncomingMessage, res: ServerResponse) {
-  return handleApiRequest(req, res);
-}
